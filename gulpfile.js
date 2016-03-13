50dfb85ae6f8f174c24f5c018ea799f7f4a478ee
beginor <beginor@qq.com>
2016-03-13 08:53:52 +0800

    gulp build task

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    tsc = require('gulp-typescript'),
+    sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('ts:app', function () {
+    var tsResult = gulp.src('app/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(tsc({
+            "target": "es5",
+            "module": "system",
+            "moduleResolution": "node",
+            "sourceMap": true,
+            "inlineSourceMap": false,
+            "emitDecoratorMetadata": true,
+            "experimentalDecorators": true,
+            "removeComments": false,
+            "noImplicitAny": false
+        }));
+    console.log(tsResult);
+    return tsResult.js.pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('dev', function () {
+    gulp.watch('app/**/*.ts', function () {
+       gulp.start('ts:app');
+    });
+});
