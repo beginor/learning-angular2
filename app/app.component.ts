b112077dad088613a0a567dd0b325dadbcbe7921
beginor <beginor@qq.com>
2016-03-13 13:09:08 +0800

    router to demos.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,25 @@
 import { Component } from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import { TabsDemo } from './demos/tabs-demo';
 
 @Component({
     selector: 'my-app',
     template: `
         <h2>Hello, Angular2</h2>
-        <button (click)="doTest()">Click me.</button>
-    `
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link active" [routerLink]="['TabsDemo']">Tabs</a>
+            </li>
+        </ul>
+        <router-outlet></router-outlet>
+    `,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS]
 })
+@RouteConfig([
+    { path: '/tabs', name: 'TabsDemo', component: TabsDemo }
+])
 export class AppComponent {
-
-    constructor() {
-
-    }
-
-    doTest() {
-        alert('You have clicked a button');
-    }
-
+    
 }
