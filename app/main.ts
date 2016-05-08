32249a2142ea764a8aefe0335f0e36c9ce308aba
beginor <beginor@qq.com>
2016-05-08 12:03:48 +0800

    use hash location strategy for live reload

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,14 @@
 /// <reference path="../typings/browser.d.ts" />
 
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { ROUTER_PROVIDERS } from '@angular/router';
+// Add these symbols to override the `LocationStrategy`
+import { provide }              from '@angular/core';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    provide(LocationStrategy, { useClass: HashLocationStrategy })
+]);
