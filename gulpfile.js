ae9bb0d9af57964accfc715508c34965e4e74a02
beginor <beginor@qq.com>
2016-06-19 15:15:10 +0800

    update gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,52 @@
-var gulp = require('gulp'),
-    tsc = require('gulp-typescript'),
+'use strict';
+
+let gulp = require('gulp'),
+    gls = require('gulp-live-server'),
+    sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    gls = require('gulp-live-server');
+    tsc = require('gulp-typescript');
+
+let root = './',
+    ts = root + 'app/**/*.ts',
+    scss = root + 'app/**/*.scss',
+    html = root + 'app/**/*.html',
+    dist = root + 'dist',
+    port = 3000;
 
 gulp.task('tsc', function () {
-    'use strict';
     var proj = tsc.createProject('tsconfig.json');
-    gulp.src('app/**/*.ts')
+    gulp.src(ts)
         .pipe(sourcemaps.init())
         .pipe(tsc(proj))
         .js
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(sourcemaps.write(root))
+        .pipe(gulp.dest(dist));
 });
 
-gulp.task('build', ['tsc']);
+gulp.task('sass', function () {
+    gulp.src(scss)
+        .pipe(sass())
+        .pipe(gulp.dest(dist))
+});
+
+gulp.task('copy', function () {
+    gulp.src(html)
+        .pipe(gulp.dest(dist));
+});
+
+gulp.task('build', ['tsc', 'sass', 'copy']);
 
 gulp.task('serve', function () {
-    var server = gls.static('./', 3000);
+    var server = gls.static(root, port);
     server.start();
     
-    gulp.watch(['./**/*.css', './**/*.js', './**/*.html'], function (file) {
+    gulp.watch([dist + '/**/*.css', dist + '/**/*.js', dist + '/**/*.html'], function (file) {
         server.notify.apply(server, [file]);
     });
 });
 
-gulp.task('dev', function () {
-    gulp.watch('app/**/*.ts', function () {
-       gulp.start('tsc');
-    });
-    gulp.start('serve');
+gulp.task('dev', ['build', 'serve'], function () {
+    gulp.watch(ts, ['tsc']);
+    gulp.watch(scss, ['sass']);
+    gulp.watch(html, ['copy']);
 });
