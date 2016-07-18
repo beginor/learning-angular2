89ccb4b2004c0215910239c55039ff6f13f0f3fe
beginor <beginor@qq.com>
2016-07-19 07:56:02 +0800

    ng-bootstrap demos.

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,11 +1,11 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 
-import { Home } from './demos/home/home';
-import { ButtonsDemo } from './demos/buttons/buttons';
+import { Home } from './demos/home';
+import { AccordionDemo } from './demos/accordion';
 
 export const routes: RouterConfig = [
     { path: '', component: Home },
-    { path: 'buttons', component: ButtonsDemo }
+    { path: 'accordion', component: AccordionDemo }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
