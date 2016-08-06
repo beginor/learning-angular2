23a55539897fcc04fb5946a29e942a1423a8118b
beginor <beginor@qq.com>
2016-08-06 18:01:42 +0800

    update package and task

--- systemjs.config.js
+++ systemjs.config.js
@@ -26,9 +26,7 @@
     'http',
     'platform-browser',
     'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
-    'upgrade',
+    'router'
   ];
   // Individual files (~300 requests):
   function packIndex(pkgName) {
