525000071b0fce9291be16fe996c8aefd75da10b
beginor <beginor@qq.com>
2016-08-28 16:54:59 +0800

    update views.

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -3,12 +3,12 @@ import { Route, RouterModule } from '@angular/router';
 import { Home } from './demos/home';
 import { AccordionDemo } from './demos/accordion';
 import { AlertDemo } from './demos/alert';
-// import { ButtonsDemo } from './demos/buttons';
-// import { CarouselDemo } from './demos/carousel';
-// import { CollapseDemo } from './demos/collapse';
-// import { DropdownDemo } from './demos/dropdown';
-// import { PaginationDemo } from './demos/pagination';
-// import { PopoverDemo } from './demos/popover';
+import { ButtonsDemo } from './demos/buttons';
+import { CarouselDemo } from './demos/carousel';
+import { CollapseDemo } from './demos/collapse';
+import { DropdownDemo } from './demos/dropdown';
+import { PaginationDemo } from './demos/pagination';
+import { PopoverDemo } from './demos/popover';
 
 export interface TextRoute extends Route {
     text: string;
@@ -18,12 +18,12 @@ export const routes: TextRoute[] = [
     { path: '', component: Home, text: 'Home' },
     { path: 'accordion', component: AccordionDemo, text: 'Accordion' },
     { path: 'alert', component: AlertDemo, text: 'Alert' },
-    // { path: 'buttons', component: ButtonsDemo, text: 'Buttons' },
-    // { path: 'carousel', component: CarouselDemo, text: 'Carousel' },
-    // { path: 'collapse', component: CollapseDemo, text: 'Collapse' },
-    // { path: 'dropdown', component: DropdownDemo, text: 'Dropdown' },
-    // { path: 'pagination', component: PaginationDemo, text: 'Pagination' },
-    // { path: 'popover', component: PopoverDemo, text: 'Popover' }
+    { path: 'buttons', component: ButtonsDemo, text: 'Buttons' },
+    { path: 'carousel', component: CarouselDemo, text: 'Carousel' },
+    { path: 'collapse', component: CollapseDemo, text: 'Collapse' },
+    { path: 'dropdown', component: DropdownDemo, text: 'Dropdown' },
+    { path: 'pagination', component: PaginationDemo, text: 'Pagination' },
+    { path: 'popover', component: PopoverDemo, text: 'Popover' }
 ];
 
 export const AppRoutes = RouterModule.forRoot(routes, { useHash: true });
