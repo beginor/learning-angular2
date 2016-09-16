c24787d7e3d16bc6de6bce7f8e77ce8fe8650e0c
beginor <beginor@qq.com>
2016-09-16 14:52:02 +0800

    remove enableProdMode

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 /// <reference path="../typings/index.d.ts" />
 
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
-
-enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .then(val => {
