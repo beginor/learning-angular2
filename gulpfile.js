354c224a6c1b3d622ca5e58907d104224dab6edd
beginor <beginor@qq.com>
2016-09-16 14:53:01 +0800

    replace gulp-live-server with gulp-connect & gulp-api-proxy, add gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 'use strict';
 
 let gulp = require('gulp'),
-    gls = require('gulp-live-server'),
+    apiProxy = require('gulp-api-proxy'),
+    connect = require('gulp-connect'),
+    sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     tsc = require('gulp-typescript');
 
 let root = './',
     ts = root + 'src/**/*.ts',
+    scss = root + 'src/**/*.scss',
     html = root + 'src/**/*.html',
     dist = root + 'dist',
     port = 3000;
@@ -18,26 +21,45 @@ gulp.task('tsc', function () {
         .pipe(tsc(proj))
         .js
         .pipe(sourcemaps.write(root))
-        .pipe(gulp.dest(dist));
+        .pipe(gulp.dest(dist))
+        .pipe(connect.reload());
 });
 
 gulp.task('copy', function () {
     gulp.src(html)
-        .pipe(gulp.dest(dist));
+        .pipe(gulp.dest(dist))
+        .pipe(connect.reload());
 });
 
-gulp.task('build', ['tsc', 'copy']);
+gulp.task('sass', function () {
+    gulp.src(scss)
+        .pipe(sass())
+        .pipe(gulp.dest(dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('build', ['tsc', 'copy', 'sass']);
 
 gulp.task('serve', function () {
-    var server = gls.static(root, port);
-    server.start();
-    
-    gulp.watch([dist + '/**/*.css', dist + '/**/*.js', dist + '/**/*.html'], function (file) {
-        server.notify.apply(server, [file]);
+    //var server = gls.static(root, port);
+    //server.start();
+    connect.server({
+        name: 'Connect Dev Server',
+        root: root,
+        port: port,
+        livereload: true,
+        // api proxy middleware
+        middleware: function (conn, opts) {
+            opts.route = '/rest';
+            opts.context = '127.0.0.1:8080/rest';
+            var proxy = new apiProxy(opts);
+            return [proxy]
+        }
     });
 });
 
-gulp.task('dev', ['build', 'serve'], function () {
+gulp.task('dev', ['build', 'serve', 'sass'], function () {
     gulp.watch(ts, ['tsc']);
     gulp.watch(html, ['copy']);
+    gulp.watch(scss, ['sass']);
 });
